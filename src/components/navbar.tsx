1ce2c2f9aae1efedfe4899d55caf3311ba94212a
Voitila Araujo <voitilaaraujo@gmail.com>
2024-04-15 00:23:12 -0300

    add github icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { useEffect, useState } from 'react';
 
 import { useTheme } from 'next-themes';
@@ -10,55 +11,58 @@ import { Link } from 'next-view-transitions';
 import { cn } from '@/lib/utils';
 
 function NavBar() {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	return (
-		<div className='mb-12 flex h-[100px] w-full items-center justify-center'>
-			<div className='flex-start  flex h-full w-full max-w-[1080px] items-center justify-start px-12'>
-				<Link
-					className={cn(
-						'mr-auto font-mono underline-offset-2 hover:underline',
-						pathname === '/'
-							? 'pointer-events-none text-neutral-400 dark:text-zinc-600'
-							: 'cursor-pointer'
-					)}
-					href='/'
-				>
-					root<span className={cn(pathname === '/' ? 'opacity-100' : 'opacity-0')}>.</span>
-				</Link>
+  return (
+    <div className='mb-12 flex h-[100px] w-full items-center justify-center'>
+      <div className='flex-start gap-4 flex h-full w-full max-w-[1080px] items-center justify-start px-12'>
+        <Link
+          className={cn(
+            'mr-auto font-mono underline-offset-2 hover:underline',
+            pathname === '/'
+              ? 'pointer-events-none text-neutral-400 dark:text-zinc-600'
+              : 'cursor-pointer'
+          )}
+          href='/'
+        >
+          root<span className={cn(pathname === '/' ? 'opacity-100' : 'opacity-0')}>.</span>
+        </Link>
 
-				<ThemeSwitch />
-			</div>
-		</div>
-	);
+        <a href='https://github.com/voitaraujo/playground' rel='noreferrer'>
+          <GitHubLogoIcon className="w-6 h-6"/>
+        </a>
+        <ThemeSwitch />
+      </div>
+    </div>
+  );
 }
 
 function ThemeSwitch() {
-	const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = useState(false);
 
-	const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
-	useEffect(() => {
-		setMounted(true);
-	}, []);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-	return (
-		<select
-			className='bg-transparent font-mono outline-none'
-			value={theme || ''}
-			onChange={(e) => setTheme(e.target.value)}
-		>
-			{mounted ? (
-				<>
-					<option value='system'>System</option>
-					<option value='dark'>Dark</option>
-					<option value='light'>Light</option>
-				</>
-			) : (
-				<option value=''></option>
-			)}
-		</select>
-	);
+  return (
+    <select
+      className='bg-transparent font-mono outline-none'
+      value={theme || ''}
+      onChange={(e) => setTheme(e.target.value)}
+    >
+      {mounted ? (
+        <>
+          <option value='system'>system</option>
+          <option value='dark'>dark</option>
+          <option value='light'>light</option>
+        </>
+      ) : (
+        <option value=''></option>
+      )}
+    </select>
+  );
 }
 
 export { NavBar };
