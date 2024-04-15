b4c2f71296da8aa6595cf23897a2f4425d3984f9
Voitila Araujo <voitilaaraujo@gmail.com>
2024-04-14 23:06:47 -0300

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { type Metadata } from 'next';
-
+import { Analytics } from '@vercel/analytics/react';
 import { type ReactNode } from 'react';
 
 import { GeistMono } from 'geist/font/mono';
@@ -15,35 +15,36 @@ import { ThemeProvider } from '@/components/theme-provider';
 import './globals.css';
 
 export const metadata: Metadata = {
-	title: 'playground | voit.dev',
-	description: 'Where I test new stuff or attempt to replicate shiny things I saw on X(Twitter).',
+  title: 'playground | voit.dev',
+  description: 'Where I test new stuff or attempt to replicate shiny things I saw on X(Twitter).',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: ReactNode;
+  children: ReactNode;
 }>) {
-	return (
-		<ViewTransitions>
-			<html
-				lang='en'
-				suppressHydrationWarning
-				className={`${GeistSans.variable} ${GeistMono.variable}`}
-			>
-				<body className={cn('bg-white font-sans text-black dark:bg-black dark:text-white')}>
-					<ThemeProvider
-						attribute='class'
-						defaultTheme='system'
-						enableSystem
-						disableTransitionOnChange
-					>
-						<NavBar />
-						{children}
-						<ScreenSize />
-					</ThemeProvider>
-				</body>
-			</html>
-		</ViewTransitions>
-	);
+  return (
+    <ViewTransitions>
+      <html
+        lang='en'
+        suppressHydrationWarning
+        className={`${GeistSans.variable} ${GeistMono.variable}`}
+      >
+        <body className={cn('bg-white font-sans text-black dark:bg-black dark:text-white')}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <NavBar />
+            {children}
+            <Analytics />
+            <ScreenSize />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ViewTransitions>
+  );
 }
